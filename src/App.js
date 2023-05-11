271ae47de76b03311017ed05771515d6ea3ae8ed
Dung Phan <fshu.pbdn@gmail.com>
2023-05-11 12:09:48 +0700

    Adding Browser Router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 // ---
 // to: src/App.js
 // ---
-
 import React from "react";
-import Typography from "@mui/material/Typography";
-import { Box, Link } from "@mui/material";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://www.google.com.vn">
-        Thanh Dung
-      </Link>{" "}
-      3{new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+import Router from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <>
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom align="center">
-          Shoes Store
-        </Typography>
-        <Copyright />
-      </Box>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
---
+++ src/pages/DetailPage.js
@@ -0,0 +1,16 @@
+// ---
+// to: src/pages/DetailPage.js
+// ---
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function DetailPage() {
+  let params = useParams();
+  return (
+    <div>
+      <h1>DetailPage - ProductId: {params.id}</h1>
+    </div>
+  );
+}
+
+export default DetailPage;
