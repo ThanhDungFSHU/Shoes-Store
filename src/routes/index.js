9bf80dc906f0a8bfb6d9ef47abdb57c60cbb0c39
Dung Phan <fshu.pbdn@gmail.com>
2023-05-11 12:34:00 +0700

    change src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@
 // to: src/routes/index.js
 // ---
 import * as React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BlankLayout from "../layouts/BlankLayout";
 import MainLayout from "../layouts/MainLayout";
 import DetailPage from "../pages/DetailPage";
